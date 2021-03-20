973be4ec62d7c770337a21cb3c340b2ce3629b1b
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 17:55:58 -0700

    adding the main loop

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ pygame.init()
 
 size = (700, 500)
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Poker Game")
+
+
+
+
+
+running = True 
+
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type  == Pygame.QUIT:
+            running = False
