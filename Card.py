5be999a822479a7df13cee4c1a1c86f10f06d19e
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 18:22:53 -0700

    added two classes

---
+++ Card.py
@@ -0,0 +1,4 @@
+class Card:
+    def __init__(self, suit, face):
+        self.suit = suit 
+        self.face = face 
---
+++ CardDeck.py
@@ -0,0 +1,12 @@
+class CardDeck:
+    faces = ["A", "2", "3", "4", "5",
+             "6", "7", "8", "9", "10",
+             "J", "Q", "K"]
+    suit = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+
+
+
+    __init__(self):
+        self.cards = []
+    
