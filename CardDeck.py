5c64f92450a3d282c2c9ca2865d011416cf9fa53
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 18:29:09 -0700

    end of session downloaded the card images

--- CardDeck.py
+++ CardDeck.py
@@ -1,3 +1,5 @@
+import random 
+
 class CardDeck:
     faces = ["A", "2", "3", "4", "5",
              "6", "7", "8", "9", "10",
@@ -9,4 +11,17 @@ class CardDeck:
 
     def __init__(self):
         self.cards = []
+        # HW: using the lists above geneate all the 52 cards 
+        # and .append() it to self.cards
+
     
+    def shuffle (self):
+        """
+        shuffles the self.card which contains all the cards
+        """
+        random.shuffle(self.cards)
+
+
+    
+
+
