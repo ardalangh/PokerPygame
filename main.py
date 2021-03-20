cab9d66084ed18e7c24539d3f952b6103818fd28
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-19 18:02:04 -0700

    bg implemented

--- main.py
+++ main.py
@@ -15,7 +15,16 @@ running = True
 
 clock = pygame.time.Clock()
 
+bg = pygame.image.load("./assets/bg.jpeg")
+
+
 while running:
     for event in pygame.event.get():
         if event.type  == Pygame.QUIT:
             running = False
+
+    screen.blit(image, (0,0))
+
+    pygame.display.flip()
+
+    clock.tick(60)
