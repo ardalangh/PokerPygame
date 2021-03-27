33b1219eb8db28e7d436a82968a40165b89b221a
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-26 18:02:15 -0700

    draw method of Crad class

--- Card.py
+++ Card.py
@@ -1,6 +1,15 @@
+import pygame
+
+
 class Card:
     def __init__(self, suit, face):
         self.suit = suit 
         self.face = face
-        self.filePath = f"/assets/cards/ {self.face} {self.suit[0].upper()}.jpg"
+        self.filePath = f"./assets/cards/ {self.face} {self.suit[0].upper()}.jpg"
+        self.size = (None , None) # width : 0 height : 1
+
+
+    def draw(self, screen, x , y):
+        card = pygame.image.load(self.filePath)
+        screen.blit(card, [x, y])
 
